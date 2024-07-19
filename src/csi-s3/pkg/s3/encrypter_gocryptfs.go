3cc28f31f25f12e89da809f593f9268e730afae5
Vlad Marchuk <marchukvl02@gmail.com>
2024-07-19 23:05:38 +0300

    feat: Gocryptfs init on temp folder and copy init data to target

--- src/csi-s3/pkg/s3/encrypter_gocryptfs.go
+++ src/csi-s3/pkg/s3/encrypter_gocryptfs.go
@@ -42,17 +42,35 @@ func (enc *gocryptfsEncrypter) MountEncrypt(source string, target string, pass s
 }
 
 func (enc *gocryptfsEncrypter) initialize(target string, passFile string) error {
+	targetDir := filepath.Dir(target)
+	tempInitDirPath := filepath.Join(targetDir, "temp-init")
+
+	CreateFolderIfNotExists(tempInitDirPath)
+
+	// delete tempInitDirPath with all the content in any case
+	defer func() {
+		exec.Command("rm", "-rf", tempInitDirPath)
+	}()
+
 	args := []string{
 		"-init",
 		"-passfile", passFile,
-		target,
+		tempInitDirPath,
 	}
-
 	cmd := exec.Command(gocryptfsCmd, args...)
-
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		return fmt.Errorf("error gocryptfs initialize command: %s\nargs: %s\noutput: %s", gocryptfsCmd, args, out)
+	}
+
+	copyCommandArgs := []string{
+		tempInitDirPath + "/*",
+		target,
+	}
+	cmd = exec.Command("cp", copyCommandArgs...)
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("error gocryptfs copy to target path on init, nargs: %s\noutput: %s", args, out)
 	}
 
 	return nil
