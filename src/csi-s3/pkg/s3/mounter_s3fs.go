b2a9a98789cb89dc8dd6ad485fe8a190be56fd74
Vlad Marchuk <marchukvl02@gmail.com>
2024-08-22 16:24:05 +0300

    feat(s3fs-mounter): Added sync and syncdir flags

--- src/csi-s3/pkg/s3/mounter_s3fs.go
+++ src/csi-s3/pkg/s3/mounter_s3fs.go
@@ -60,6 +60,7 @@ func (s3fs *s3fsMounter) Mount(target string) error {
 		"-o", fmt.Sprintf("url=%s", s3fs.url),
 		"-o", "allow_other",
 		"-o", "compat_dir",
+		"-o", "sync,dirsync",
 	}
 
 	return fuseMount(target, s3fsCmd, args)
